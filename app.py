b201d7d9e1bc53123d959b9e69b3b5f7f35c660f
Rohit Gupta <rohit.ajaygupta@gmail.com>
2021-05-29 19:31:40 +0530

    Added basic tests for status code

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 import logging
+import logging.config
 import os
 import smtplib
-import json
 import psycopg2
 import psycopg2.extras
 import config
+import simplejson as json
 
 from flask import Flask, request, jsonify
 from src import twitter_utils
 from src import db_utils as db
 from src.send_email import send_mail
 from pretty_html_table import build_table
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(lineno)d: %(levelname)s :%(asctime)s:%(name)s:%(message)s")
+
+file_handler = logging.FileHandler("app.log")
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+# from src.logger import setup_logger
+# alog = setup_logger("app_logger", "./logs/app_logfile.log")
 
 
 app = Flask(__name__)
@@ -19,61 +36,80 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def get_homepage():
     return jsonify({"Result": "Success"})
+    logger.info("homepage successful")
 
 @app.route("/get_urls", methods=['GET'])
 def get_urls():
-    # call database read function to get data urls for each member in database
-    list_urls = db.get_url()
+    # data = request.get_json()
+    # print(data)
+    # for member in data:
+    #     twitter_id = member["twitter_id"]
+    # logger.info(f"{data} is our data for get_urls")
     result = {"result": []}
-    for linkedin, twitter in list_urls:
-        result["result"].append({"linkedin": linkedin, "twitter": twitter})
+    list_urls = db.get_url(twitter_id = None)
+    for id, twitter,linkedin in list_urls:
+        result["result"].append({"id": id, "linkedin": linkedin, "twitter": twitter})
+    logger.info("urls read successfully")
     return jsonify(result=result)
 
 
-@app.route("/add_sentiment", methods=["GET"])
+@app.route("/add_sentiment", methods=[ "POST"])
 def add_sentiment():
     list_sentiments = db.update_twitter_sentiment_from_ids()
+    logger.info(f"status of sentiments {list_sentiments}")
     if list_sentiments[0] == 1:
         result = {"status": "success"}
     else:
         result = {"status": "failure"}
+    logger.info("Added Sentiments successfully")
     return jsonify(result)
 
 
-@app.route("/add_members", methods=['GET'])
+@app.route("/add_members", methods=['POST'])
 def add_members():
     data = request.get_json()
-    id = data["id"]
-    name = data["name"]
-    twitter_id = data["twitter_id"]
-    linkedin_url = data["linkedin_url"]
+    result = []
+    for member in data:
+        id = member["id"]
+        name = member["name"]
+        twitter_id = member["twitter_id"]
+        linkedin_url = member["linkedin_url"]
 
-    # call database create/update function to add members
-    add_members = db.insert_data(id, name, twitter_id, linkedin_url)
-    if add_members == 1:
-        result = {"status": "success"}
-    else:
-        result = {"status": "failure"}
+        # call database create/update function to add members
+        add_member = db.insert_data(id, name, twitter_id, linkedin_url)
+
+        if add_member == 1:
+            result.append({"status": "success"})
+            logger.info(f"{member} added successfully")
+        else:
+            result.append({"status": "failure"})
+            logger.error(f"{member} couldn't be added.")
 
     return jsonify(result)
 
 
-@app.route("/get_members", methods=['GET'])
-def read_members():
-    list_members= db.query_db("SELECT * FROM Employee")
-    print(list_members)
-    return jsonify(list_members)
+@app.route("/get_members", methods=['POST'])
+def get_members():
+    try:
+        list_members= db.read_record(twitter_id= None)
+        logger.info(f'{list_members} are shown from Employee table')
+
+    except Exception as e:
+        logger.error(f"{e}")
+    return json.dumps(list_members,use_decimal= True)
+    # return jsonify(list_members)
 
 
 @app.route("/notify", methods=["GET"])
 def notify():
     sentiment_data = db.read_name_sentiment()
-    print("sentiment_data", sentiment_data)
+    logger.info(f"mailing sentiment data for {sentiment_data}")
     try:
         output = build_table(sentiment_data, 'blue_light')
         send_mail(output)
+        logger.info("Mail sent successfully")
     except Exception as error:
-        print(error)
+        logger.error(f"{error}")
 
     return "Mail sent successfully."
 
--- src/db_utils.py
+++ src/db_utils.py
@@ -6,6 +6,28 @@ import pandas as pd
 import psycopg2.extras
 from configparser import ConfigParser
 from src.twitter_utils import tweet_user
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+
+file_handler = logging.FileHandler("db_utils.log")
+# file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+
+# stream_handler = logging.StreamHandler()
+# stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+# logger.addHandler(stream_handler)
+
+# from src.dictConfig_logger import configure_logger
+# alog = configure_logger('default','..\logs\db_log.log')
+# from src.logger import setup_logger
+#
+# dblog = setup_logger("db_logger", "../logs/db_logfile.log")
 
 
 def conn():
@@ -18,6 +40,7 @@ def conn():
     )
     conn.autocommit = True
     cursor = conn.cursor()
+    logger.info("postgres connection Successful")
     return conn, cursor
 
 conn, cursor = conn()
@@ -45,13 +68,14 @@ def create_table():
 
 def insert_data(id,name,twitter_id,linkedin_url = False):
 
-    twitter_sentiment = tweet_user(twitter_id)
+    twitter_sentiment = tweet_user(twitter_id,max_tweets= 1)
     linkedin_url = False
     conn.autocommit = True
     try:
         cursor.execute("INSERT INTO Employee(id,name,twitter_id,linkedin_url) VALUES (%s,%s,%s,%s)",(id,name,twitter_id,linkedin_url))
     except:
         return 0
+        # logger.error("Insert Query unsuccessful.Please add new values.")
     return 1
 
 #Read Table
@@ -60,16 +84,43 @@ def read_records():
     sql = "SELECT * FROM Employee;"
     cursor.execute(sql)
     employees = cursor.fetchall()
+    logger.info("read table")
     return employees
 
+def read_record(twitter_id):
+    if twitter_id is None:
+        res = cursor.execute("SELECT * FROM Employee")
+        urls = cursor.fetchall()
+        logger.info("all employees fetched")
+        # print(urls)
+        return urls
+    else:
+        res = cursor.execute("SELECT * FROM Employee WHERE id = %s",(twitter_id))
+        urls = cursor.fetchall()
+        logger.info(f"url for {twitter_id} fetched ")
+        # print(urls)
+        return urls
 
-def get_url():
+# print(read_record(twitter_id= None))
+
+
+def get_url(twitter_id):
     """reading the social media ids from the table"""
-    sql = "SELECT twitter_id,linkedin_url FROM Employee"
-    cursor.execute(sql)
-    urls = cursor.fetchall()
-    return urls
+    if twitter_id is None:
+        res = cursor.execute("SELECT id, twitter_id,linkedin_url FROM Employee ")
+        urls = cursor.fetchall()
+        logger.info("all urls fetched")
+        # print(urls)
+        return urls
+    else:
+        res = cursor.execute("SELECT twitter_id,linkedin_url FROM Employee WHERE id = %s",(id_info))
+        urls = cursor.fetchall()
+        logger.info(f"url for {twitter_id} fetched ")
+        # print(urls)
+        return urls
 
+# print(get_url(twitter_id = None))
+# print((get_url(twitter_id = "1")))
 
 def read_name_sentiment():
     """Reading id, name and sentiment of employee from the table"""
@@ -77,6 +128,7 @@ def read_name_sentiment():
     s = """SELECT id,name,twitter_sentiment from Employee;"""
     cursor.execute(s)
     employee = cursor.fetchall()
+    # ilog.info(f"The sentiments of the employees are {employee}")
     conn.commit()
 
     df = pd.DataFrame(employee[1:], columns=("id", "name", "sentiment"))
@@ -92,15 +144,46 @@ def update_twitter_sentiment_from_ids():
     conn.commit()
     # return twitter_ids
     list = [update_tweet_sentiment(id[0]) for id in twitter_ids]
+    logger.info(f"list of updated twitter sentiments - {list}")
+    return list
+
+#function useful for API
+def update_twitter_sentiment_pattern_from_ids():
+    """updating and checking the update of twitter sentiment for the user"""
+    s = "SELECT twitter_id FROM Employee"
+    max_tweets = 15
+    cursor.execute(s)
+    twitter_ids = cursor.fetchall()
+    conn.commit()
+    # return twitter_ids
+    list = [get_twitter_sentiment_pattern_from_ids(id[0]) for id in twitter_ids]
+    logger.info(f"list of updated twitter sentiments - {list}")
     return list
 
 
+
+def get_twitter_sentiment_pattern_from_ids(twitter_id):
+    twitter_sentiment_pattern = tweet_user(twitter_id,max_tweets= 15)
+    print(twitter_sentiment_pattern)
+    try:
+        cursor.execute("""UPDATE Employee SET twitter_sentiment_pattern = %s WHERE twitter_id = %s""",(twitter_sentiment_pattern,twitter_id))
+        logger.info(f"twitter sentiment pattern for {twitter_id} updated successfully")
+    except Exception as error:
+        logger.error(f"{error}")
+        return 0
+    return 1
+
+# a = update_twitter_sentiment_pattern_from_ids()
+# print(a)
+
+
 def update_tweet_sentiment(twitter_id):
-    twitter_sentiment = (tweet_user(twitter_id))
+    twitter_sentiment = (tweet_user(twitter_id,1))
     try:
         cursor.execute("""UPDATE Employee SET twitter_sentiment = %s WHERE twitter_id = %s """,(twitter_sentiment, twitter_id))
+        logger.info(f"twitter sentiment for {twitter_id} updated successfully")
     except Exception as error:
-        print(str(error))
+        logger.error(f"{error}")
         return 0
     return 1
 
@@ -108,14 +191,14 @@ def update_tweet_sentiment(twitter_id):
 def update_twitter_linkedin_sentiment(twitter_id,linkedin_sentiment = False):
     twitter_sentiment = tweet_user(twitter_id)
     linkedin_sentiment = False
-    print(twitter_sentiment)
+    logger.info(f"{twitter_sentiment} to be updated")
     try:
-
         cursor.execute("INSERT INTO Employee (twitter_sentiment,linkedin_sentiment) VALUES (%f, %s)",(twitter_sentiment,linkedin_sentiment))
         conn.commit()
         twitter_sentiment = tweet_user(twitter_id)
-        print(twitter_sentiment)
-    except:
+        logger.info(f"twitter sentiment for id = {twitter_id} ie {twitter_sentiment} updated.")
+    except Exception as error:
+        logger.error(f"{error}")
         return 0
     return 1
 
@@ -126,25 +209,33 @@ def delete_records(name):
     try:
         # conn.autocommit = True
         cursor.execute(sql_delete)
+        logger.info(f"{name} successfully deleted from Employee.")
     except:
+        logger.warn(f"could not delete {name} from Employee.")
         return 0
     return 1
 
 #Other useful functions
 
-def query_db(query, args=(), one=False):
+def query_db(query,args=(), one=False):
     """returns table in a list of dicts"""
+    # query = "SELECT * FROM Employee"
     cursor.execute(query, args)
-    r = [dict((cursor.description[i][0], value) \
-               for i, value in enumerate(row)) for row in cursor.fetchall()]
+    try:
+        r = [dict((cursor.description[i][0], value) \
+                for i, value in enumerate(row)) for row in cursor.fetchall()]
+        a
+    except Exception as e:
+        logger.error(f"{e}")
     # cursor.connection.close()
+    logger.info(f"{r} successful")
     return (r[0] if r else None) if one else r
 
 
 def alter_table():
     """ to add new column or delete a column to and from the table"""
-    cursor.execute("""ALTER TABLE Employee
-                     ADD COLUMN twitter_sentiment NUMERIC(5,2); """)
+    cursor.execute("""ALTER TABLE Employee ADD COLUMN twitter_sentiment_pattern VARCHAR(30); """)
+    logger.info("Alter table successful")
 
-
+# alter_table()
 
